771a6a74f05d237e16f675bf9fc09b0643990f89
mrcyclo <vipboysanhdieu@gmail.com>
2025-09-09 15:37:29 +0700

    Add Harmony patch to track player death count
    Introduces a new DeathCount patch that hooks into HeroController's Awake method. Increments the death count configuration value each time the player dies.

---
+++ Patches/DeathCount.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace SilksongUtils.Patches
+{
+    internal class DeathCount
+    {
+        [HarmonyPatch(typeof(HeroController), "Awake")]
+        [HarmonyPostfix]
+        private static void Awake_Postfix(HeroController __instance)
+        {
+            __instance.OnDeath += () => Plugin.configDeathCount.Value += 1;
+        }
+    }
+}
