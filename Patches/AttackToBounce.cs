b9a52de30080181e5acaa2106c1cf07c8082b5e6
mrcyclo <vipboysanhdieu@gmail.com>
2025-09-10 14:55:23 +0700

    Refactor AttackToBounce to use HitResponded event
    Replaces the BouncePod Hit prefix patch with a postfix patch on DamageEnemies.Awake that subscribes to the HitResponded event. Adds a HarmonyReversePatch for HeroDownAttack.OnHitResponded and updates logic to trigger bounce behavior when appropriate. This refactor improves modularity and leverages event-driven handling.

--- Patches/AttackToBounce.cs
+++ Patches/AttackToBounce.cs
@@ -1,20 +1,33 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace SilksongUtils.Patches
 {
     internal class AttackToBounce
     {
-        [HarmonyPatch(typeof(BouncePod), "Hit")]
-        [HarmonyPrefix]
-        private static void BouncePod_Hit_Prefix(BouncePod __instance, ref HitInstance damageInstance)
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(HeroDownAttack), "OnHitResponded")]
+        public static void HeroDownAttack_OnHitResponded(object instance, DamageEnemies.HitResponse hitResponse) => throw new System.NotImplementedException();
+
+        [HarmonyPatch(typeof(DamageEnemies), "Awake")]
+        [HarmonyPostfix]
+        public static void DamageEnemies_ProcessDamageBuffer_Postfix(DamageEnemies __instance)
         {
-            if (!Plugin.configAttackToBounce.Value) return;
-            if (damageInstance.AttackType != AttackTypes.Nail) return;
+            __instance.HitResponded += (response) =>
+            {
+                if (!Plugin.configAttackToBounce.Value) return;
 
-            var hitDirection = damageInstance.GetHitDirection(HitInstance.TargetType.BouncePod);
-            if (hitDirection == HitInstance.HitDirection.Down) return;
+                var hitDirection = response.Hit.GetHitDirection(HitInstance.TargetType.BouncePod);
+                if (hitDirection == HitInstance.HitDirection.Down) return;
 
-            damageInstance.Direction = 300;
+                var hc = HeroController.instance;
+                if (hc.cState.onGround) return;
+
+                var heroDownAttack = Object.FindAnyObjectByType<HeroDownAttack>();
+                if (heroDownAttack == null) return;
+
+                HeroDownAttack_OnHitResponded(heroDownAttack, response);
+            };
         }
     }
 }
